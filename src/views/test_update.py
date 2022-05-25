5dbd0e454d25f90afecbc705e3fb32e42a5af1d9
Nlelyavin <nlelyavin1@yandex.ru>
2022-05-25 06:10:34 +0300

    Обновление значения поля работает

--- src/views/test_update.py
+++ src/views/test_update.py
@@ -2,4 +2,4 @@ from src.json_rpc import JsonRPC
 
 if __name__ == '__main__':
     json_rpc = JsonRPC()
-    response = json_rpc.update_value_fields(value_fields=[{1: 'asdasd'}])
+    response = json_rpc.update_value_fields(value_fields=[{"id": 1, "value_field": "str"}])
